61e6b5f3e71f5c0ef2ed06379d48cbc5cc59041e
Alvar0ne <elxalvaro@gmail.com>
2017-11-16 00:37:38 -0300

    Agregado codigo lectura de puntos y guardado en estructura

--- Tarea3CoordenadasEnEstructura.cpp
+++ Tarea3CoordenadasEnEstructura.cpp
@@ -4,12 +4,32 @@
 #include <fstream>
 #include <limits.h>
 #include <cstring>
+#include <math.h>
 
 using namespace std;
 typedef char cadena[30];
 
+typedef struct
+{
+  float x;
+  float y;
+}punto;
+
+float distancia_xy (float x, float y)
+{
+  return sqrt(pow(x,2)+pow(y,2));
+}
+
+punto buscar_punto(int indice, punto coordenada[])
+{
+  return coordenada[indice];
+}
+
+
+
 int main(int argc, char *argv[])
 {
+  punto coordenada[5000];
 	char *ptr;
     if(argc<2)    cout<<"Ingrese la ruta del archivo como argumento. Ejemplo: \"./tarea1 numeros.csv\""<<endl;
     else{
@@ -23,16 +43,30 @@ int main(int argc, char *argv[])
 
            	while(!fs.eof())
 	        {
+              int cont2=0;
            		cont++;
            		fs.getline(string,30,'\n');
-           		cout<<"Dato sin separar: "<<string<<endl; 
+           		//cout<<"Dato sin separar: "<<string<<endl; 
            		ptr = strtok(string," ");
            		while(ptr != NULL)
                    {
-                    cout <<"Dato separado:"<< ptr << endl;
+
+                    //cout <<"Dato separado:"<< ptr << endl;
+                    float d=atof(ptr);
+                    //cout <<"Dato separado en FLOAT:"<< d << endl;
+                    if (cont2==1){
+                    coordenada[cont].x = d;
+                      }
+                    if (cont2==2){
+                    coordenada[cont].y = d;
+                     }
                     ptr = strtok(NULL, " ");
+                    cont2++;
                    }
            	}
+            //cout <<"DATO X EN STRUC 4999 PRUEBA:"<< coordenada[4999].x << endl;
+            //cout <<"DATO Y EN STRUC 9 PRUEBA:"<< coordenada[9].y << endl;
+            //cout <<"DATO X EN STRUC 3 PRUEBA:"<< coordenada[3].x << endl;
 
            	int lineas= cont-1;
            	cout<<"Lineas en el fichero: "<<lineas<<endl; 
